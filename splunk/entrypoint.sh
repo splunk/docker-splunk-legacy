d6a6e08a26de8cbc497a774343fb9935607c4008
Denis Gladkikh <denis@gladkikh.email>
2015-08-20 23:26:01 -0700

    Fix misprint in envrinment name

--- splunk/entrypoint.sh
+++ splunk/entrypoint.sh
@@ -17,7 +17,7 @@ elif [ "$1" = 'start-service' ]; then
   sudo -HEu ${SPLUNK_USER} ${SPLUNK_HOME}/bin/splunk start --accept-license --answer-yes --no-prompt
   trap "sudo -HEu ${SPLUNK_USER} ${SPLUNK_HOME}/bin/splunk stop" SIGINT SIGTERM EXIT
 
-  if [[ -n ${SPLUNK_FORAWARD_SERVER} ]]; then
+  if [[ -n ${SPLUNK_FORWARD_SERVER} ]]; then
     if sudo -HEu ${SPLUNK_USER} ${SPLUNK_HOME}/bin/splunk list forward-server -auth admin:changeme | grep -q "${SPLUNK_FORWARD_SERVER}"; then
       sudo -HEu ${SPLUNK_USER} ${SPLUNK_HOME}/bin/splunk add forward-server "${SPLUNK_FORWARD_SERVER}" -auth admin:changeme
     fi

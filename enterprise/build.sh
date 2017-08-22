74493fcb30dde6c9f32af162214ce877cbf15f30
Maarten Hoogcarspel <maarten@hoogcarspel.net>
2017-08-22 11:03:17 +0100

    Update for splunk version 6.6.3

--- enterprise/build.sh
+++ enterprise/build.sh
@@ -3,7 +3,7 @@ if [ -z $CURRENT ]; then
 	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 fi
 
-docker build --no-cache=true -t splunk/splunk:6.6.2 .
-docker tag splunk/splunk:6.6.2 splunk/splunk:latest 
-docker tag splunk/splunk:6.6.2 registry.splunk.com/splunk/splunk:latest 
-docker tag splunk/splunk:6.6.2 registry.splunk.com/splunk/splunk:6.6.2 
+docker build --no-cache=true -t splunk/splunk:6.6.3 .
+docker tag splunk/splunk:6.6.3 splunk/splunk:latest 
+docker tag splunk/splunk:6.6.3 registry.splunk.com/splunk/splunk:latest 
+docker tag splunk/splunk:6.6.3 registry.splunk.com/splunk/splunk:6.6.3 

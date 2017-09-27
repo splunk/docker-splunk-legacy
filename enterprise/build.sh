bdac674ed7e8d7e95c22206363360f4651f6e96f
Denis Gladkikh <denis@gladkikh.email>
2017-09-26 18:55:51 -0700

    Splunk 7.0.0

--- enterprise/build.sh
+++ enterprise/build.sh
@@ -3,7 +3,7 @@ if [ -z $CURRENT ]; then
 	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 fi
 
-docker build --no-cache=true -t splunk/splunk:6.6.3 .
-docker tag splunk/splunk:6.6.3 splunk/splunk:latest 
-docker tag splunk/splunk:6.6.3 registry.splunk.com/splunk/splunk:latest 
-docker tag splunk/splunk:6.6.3 registry.splunk.com/splunk/splunk:6.6.3 
+docker build --no-cache=true -t splunk/splunk:7.0.0 .
+docker tag splunk/splunk:7.0.0 splunk/splunk:latest 
+docker tag splunk/splunk:7.0.0 registry.splunk.com/splunk/splunk:latest 
+docker tag splunk/splunk:7.0.0 registry.splunk.com/splunk/splunk:7.0.0 

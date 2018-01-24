4de008b07af641e543157d18e00bf9399a6fe06e
Michael S. Wilde <thewilde@splunk.com>
2018-01-24 14:48:51 -0600

    Merge pull request #61 from outcoldman/pr-7.0.1
    Splunk 7.0.1

--- enterprise/build.sh
+++ enterprise/build.sh
@@ -3,7 +3,7 @@ if [ -z $CURRENT ]; then
 	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 fi
 
-docker build --no-cache=true -t splunk/splunk:7.0.0 .
-docker tag splunk/splunk:7.0.0 splunk/splunk:latest 
-docker tag splunk/splunk:7.0.0 registry.splunk.com/splunk/splunk:latest 
-docker tag splunk/splunk:7.0.0 registry.splunk.com/splunk/splunk:7.0.0 
+docker build --no-cache=true -t splunk/splunk:7.0.1 .
+docker tag splunk/splunk:7.0.1 splunk/splunk:latest 
+docker tag splunk/splunk:7.0.1 registry.splunk.com/splunk/splunk:latest 
+docker tag splunk/splunk:7.0.1 registry.splunk.com/splunk/splunk:7.0.1 

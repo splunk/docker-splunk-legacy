dcd8b9d548b1d3ec0d68721afcd0ade32160d850
Denis Gladkikh <denis@gladkikh.email>
2018-04-24 22:46:30 -0700

    Splunk 7.1.0

--- universalforwarder/entrypoint.sh
+++ universalforwarder/entrypoint.sh
@@ -51,7 +51,7 @@ Splunk Forwarder
 
   Usage:
 
-    docker run -it splunk/universalforwarder:7.0.2
+    docker run -it splunk/universalforwarder:7.1.0
     docker run --env SPLUNK_START_ARGS="--accept-license" outcoldman/splunk:latest-forwarder
 
 EOF

c462496a67b41d654810aa698141735c4ba2933b
Renaud Holcombe <renaud.holcombe@hyperg0at.com>
2018-05-27 17:51:08 -0400

    Updating to version 7.1.1.

--- enterprise/build.sh
+++ enterprise/build.sh
@@ -3,7 +3,7 @@ if [ -z $CURRENT ]; then
 	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 fi
 
-docker build --no-cache=true -t splunk/splunk:7.1.0 .
-docker tag splunk/splunk:7.1.0 splunk/splunk:latest 
-docker tag splunk/splunk:7.1.0 registry.splunk.com/splunk/splunk:latest 
-docker tag splunk/splunk:7.1.0 registry.splunk.com/splunk/splunk:7.1.0 
+docker build --no-cache=true -t splunk/splunk:7.1.1 .
+docker tag splunk/splunk:7.1.1 splunk/splunk:latest 
+docker tag splunk/splunk:7.1.1 registry.splunk.com/splunk/splunk:latest 
+docker tag splunk/splunk:7.1.1 registry.splunk.com/splunk/splunk:7.1.1 
